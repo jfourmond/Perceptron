b741ca0c8d8082a77adf0dbd5568549cb45d2b4f
Jérôme FOURMOND <jerome@fourmond.fr>
2016-12-28 15:24:30 +0100

    Ajout phase de test

--- neural_network.py
+++ neural_network.py
@@ -26,8 +26,10 @@ HIDDEN_NEURONS = 10
 OUTPUT_NEURONS = 10
 # Pas d'apprentissage
 LEARNING_STEP = 0.1
-# Nombre d'itérations
-ITERATIONS = 100000
+# Nombre d'itérations d'apprentissage
+LEARNING_ITERATIONS = 1000
+# Nombre d'itérations de tests
+TEST_ITERATIONS = 10
 
 class Perceptron:
 	# Le Perceptrion multi-couches, c'est :
@@ -42,7 +44,9 @@ class Perceptron:
         self.hidden_neurons = hidden_neurons
         self.output_neurons = output_neurons
         self.nb_layers = hidden_layers + 2
+        self.learning_iteration = 0
         self.build()
+    # FIN METHODE __init__
 
     # Construction du réseau
     def build(self):
@@ -61,9 +65,13 @@ class Perceptron:
         self.nb_read = 0
         self.nb_wrong = 0
         self.nb_right = 0
+    # FIN METHODE build
 
     # Algorithme apprentissage
     def learn(self, index):
+        self.learning_iteration += 1
+        if (self.learning_iteration % 100) == 0:
+            print "Itération", self.learning_iteration
         self.errors = []
         self.outputs = []
         target = numpy.zeros(10)
@@ -73,32 +81,39 @@ class Perceptron:
         target = data[0][1][index]
         # on récupère à quel chiffre cela correspond (position du 1 dans label)
         label = numpy.argmax(target)
-
-        self.current_input = image
         # 1. Calcul de la sortie de chaque neurone i de chaque couche l du réseau par propagation couche par couche de l'activité
-        final_output = self.computeOutput(self.current_input)
-        self.current_input = numpy.append(self.current_input, 1)
-        
-        print "SORTIE FINALE :", final_output
-        print "TARGET :", target
+        final_output = self.computeOutput(image)
+        self.current_input = numpy.append(image, 1)
+        # 2. Pour chaque neurone i de la couche n de sortie, calculer l'erreur
+        self.computeOutputError(target, final_output)
+        # 3. Rétro-propager couche par couche l'erreur à travers chaque neurone i de chaque couche l du réseau
+        self.retroPropagate()
+        # 4. Modifier chaque poids
+        self.updateWeight()
+    # FIN METHODE learn
+
+    # Test du réseau
+    def test(self, index):
+        target = numpy.zeros(10)
+        # Dans la base de test (premier [1]), dans la base d'image (deuxième [0]), on récupère l'image à [index]
+        image = data[1][0][index]
+        # Dans la base de test ([1]), dans la base des labels ([1]), on récupère le label à [index]
+        target = data[1][1][index]
+        # on récupère à quel chiffre cela correspond (position du 1 dans label)
+        label = numpy.argmax(target)
+
+        final_output = self.computeOutput(image)
+        # print "SORTIE FINALE :", final_output
+        # print "TARGET :", target
         value = self.anylisis(final_output)
-        print "VALUE :", value
+        print "VALUE :", value, ", LABEL :", label
         if value == label :
             self.nb_right += 1
         else:
             self.nb_wrong += 1
         self.nb_read += 1
-        # 2. Pour chaque neurone i de la couche n de sortie, calculer l'erreur
-        self.computeOutputError(target, final_output)
-        # 3. Rétro-propager couche par couche l'erreur à travers chaque neurone i de chaque couche l du réseau
-        self.retroPropagate()
-        # print "ERRORS", self.errors
-        # 4. Modifier chaque poids
-        self.updateWeight()
-        print "LUS :", p.nb_read, "BONS :", p.nb_right, "FAUX :", p.nb_wrong
-        e = (p.nb_wrong / p.nb_read) * 100
-        print "POURCENTAGE D'ERREUR :", e
-    
+    # FIN METHODE test
+
     # Calcul de la sortie de chaque neurone i de chaque couche l du réseau par propagation couche par couche de l'activité par l'entrée "input"
     def computeOutput(self, input):
         # Ajout du biais à l'input
@@ -132,6 +147,7 @@ class Perceptron:
         self.outputs.append(last_output)
         # Retourne la sortie de la couche de sortie
         return last_output
+    # FIN METHODE computeOutput
 
     # Calcul de la sortie du neurone "neuron" de la couche "layer" par l'entrée "input"
     def sigmoid(self, layer, neuron, input):
@@ -139,6 +155,7 @@ class Perceptron:
         output = math.exp(-1. * output)
         output = 1 / (1  + output)
         return output
+    # FIN METHODE sigmoid
 
     # Calcul de l'erreur des neurones de la couche de sortie
     def computeOutputError(self, target, output_final):
@@ -148,6 +165,7 @@ class Perceptron:
             e =  y * (1 - y) * (target[i] - y) # Calcul de l'erreur du neurone i
             error.append(e)
         self.errors.insert(0, error)
+    # FIN METHODE computeOutputError
 
     # Rétro-propagation couche par couche l'erreur à travers chaque neurone i de chaque couche l du réseau
     def retroPropagate(self):
@@ -156,11 +174,10 @@ class Perceptron:
             for i in range(len(self.layers[l])):
                 weights = self.weights(l+1, i)  # Récupération des poids i des neurones de la couche d'au-dessus
                 y = self.outputs[l][i]          # Sortie du neurone courant
-                # weights = self.layers[l+1][i]   # Poids du neurone i de la couche d'au-dessus
-                # weights = weights[0:(len(weights)-1)] # Suppression du biais ?
                 delta = y*(1-y)*numpy.dot(self.errors[0], weights)
                 error.append(delta)
             self.errors.insert(0, error)
+    # FIN METHODE retroPropagate
 
     # Mise à jour de tous les poids du réseau
     def updateWeight(self):
@@ -178,14 +195,17 @@ class Perceptron:
                     # La variation du poids j, du neurone i, de la couche l 
                     variation = self.variation(l, i, j) # V_poids = poids' - poids '' -> poids'' = poids - V_poids
                     self.layers[l][i][j] = self.layers[l][i][j] + variation
+    # FIN METHODE updateWeight
 
     # Calcul de la variation du poids "weight", du neurone "neuron", de la couche "layer"
     def variation(self, layer, neuron, weight):
         return LEARNING_STEP * self.errors[layer][neuron] * self.outputs[layer-1][weight]
+    # FIN METHODE variation
 
     # Calcul de la variation du poids "weight", du neurone "neuron", de la couche d'entrée
     def variationInput(self, neuron, weight):
         return LEARNING_STEP * self.errors[0][neuron] * self.current_input[weight]
+    # FIN METHODE variationInput
 
     # Récupération d'une liste des poids "weight" des neurones de la couche "layer"
     def weights(self, layer, weight):
@@ -193,26 +213,55 @@ class Perceptron:
         for i in range(len(self.layers[layer])):
             weights.append(self.layers[layer][i][weight])
         return weights
+    # FIN METHODE weights
 
     def anylisis(self, final_output):
         return (numpy.argmax(final_output))
+    # FIN METHODE anylisis
 
     def __str__(self):
         return "" + str(self.input_neurons) + " neurones d'entrée, " + str(self.hidden_layers) + " couche(s) cachée(s) avec " \
             + str(self.hidden_neurons) + " neurones par couche, " + str(self.output_neurons) + " neurones de sortie."
+    # FIN METHODE __str__
     
     def __repr__(self):
         return self.__str__()
+    # FIN METHODE __repr__
+
+# FIN DE LA CLASSE PERCEPTRON
+
+def learningStage(Perceptron):
+    # n = nombre d'images dans le tableau d'apprentissage
+    n = numpy.shape(data[0][0])[0]
+    # on créé un vecteur de (ITERATIONS,) valeurs entières prises aléatoirement entre 0 et n-1
+    indices = numpy.random.randint(n,size=(LEARNING_ITERATIONS,))
+    # i va valoir itérativement les valeurs dans indices / NB on aurait aussi pu écrire "for j in xrange(10): i = indices[j]"
+    print "DEBUT DE LA PHASE D'APPRENTISSAGE SUR", LEARNING_ITERATIONS, "ITERATIONS"
+    for i in indices:
+        p.learn(i)
+    print "FIN DE LA PHASE D'APPRENTISSAGE"
+# FIN FONCTION learningStage
+
+def testStage(Perceptron):
+    print "DEBUT DE LA PHASE DE TEST SUR", TEST_ITERATIONS, "ITERATIONS"
+    # n = nombre d'images dans le tableau de test
+    n = numpy.shape(data[1][0])[0]
+    # on créé un vecteur de (ITERATIONS,) valeurs entières prises aléatoirement entre 0 et n-1
+    indices = numpy.random.randint(n,size=(TEST_ITERATIONS,))
+    # i va valoir itérativement les valeurs dans indices / NB on aurait aussi pu écrire "for j in xrange(10): i = indices[j]"
+    for i in indices:
+        p.test(i)
+    print "LUS :", p.nb_read, "BONS :", p.nb_right, "FAUX :", p.nb_wrong
+    e = (p.nb_wrong / p.nb_read) * 100
+    print "POURCENTAGE D'ERREUR :", e
+    print "FIN DE LA PHASE DE TEST"
+# FIN FONCTION testStage
 
 if __name__ == '__main__':
     p = Perceptron(INPUT_NEURONS, HIDDEN_LAYERS, HIDDEN_NEURONS, OUTPUT_NEURONS)
     print p
     data = cPickle.load(gzip.open('mnist.pkl.gz'))
-    # n = nombre d'images dans le tableau d'apprentissage
-    n = numpy.shape(data[0][0])[0]
-    # on créé un vecteur de (ITERATIONS,) valeurs entières prises aléatoirement entre 0 et n-1
-    indices = numpy.random.randint(n,size=(ITERATIONS,))
-    # i va valoir itérativement les valeurs dans indices / NB on aurait aussi pu écrire "for j in xrange(10): i = indices[j]"
-    for i in indices:
-        # appel de la fonction d'affichage
-        p.learn(i)
+    # Etape d'apprentissage
+    learningStage(p)
+    # Etape de test
+    testStage(p)

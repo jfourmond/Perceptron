fdae71b28182770b1a17221b0d2a68cb1eb58007
Jérôme FOURMOND <jerome@fourmond.fr>
2016-12-29 15:45:41 +0100

    Ajout instructions pour débogage

--- neural_network.py
+++ neural_network.py
@@ -18,7 +18,7 @@ import math
 INPUT = 784
 # Nombre de neurones pour la couche d'entrée
 INPUT_NEURONS = 10
-# Nombre de couches cachés (sachant que les couches "visibles" sont au nombre de deux : couche d'entrée et couche de sortie)
+# Nombre de couches cachées (sachant que les couches "visibles" sont au nombre de deux : couche d'entrée et couche de sortie)
 HIDDEN_LAYERS = 0
 # Nombre de neurones par couche cachée
 HIDDEN_NEURONS = 10
@@ -30,6 +30,10 @@ LEARNING_STEP = 0.1
 LEARNING_ITERATIONS = 100000
 # Nombre d'itérations de tests
 TEST_ITERATIONS = 1000
+
+# ... POUR DEBOGAGE ...
+LAST_VALUE = -1
+OCC = 0
 
 class Perceptron:
 	# Le Perceptrion multi-couches, c'est :
@@ -83,6 +87,12 @@ class Perceptron:
         label = numpy.argmax(target)
         # 1. Calcul de la sortie de chaque neurone i de chaque couche l du réseau par propagation couche par couche de l'activité
         final_output = self.computeOutput(image)
+        # ... POUR DEBOGAGE ...
+        value = self.anylisis(final_output)
+        if value == LAST_VALUE:
+            OCC += 1
+        else:
+            OCC = 0
         self.current_input = numpy.append(image, 1)
         # 2. Pour chaque neurone i de la couche n de sortie, calculer l'erreur
         self.computeOutputError(target, final_output)
@@ -90,6 +100,9 @@ class Perceptron:
         self.retroPropagate()
         # 4. Modifier chaque poids
         self.updateWeight()
+        # ... POUR DEBOGAGE ...
+        if OCC > 10:
+            print "DETECTION D'UN COMPORTEMENT INHABITUEL..."
     # FIN METHODE learn
 
     # Test du réseau
@@ -230,7 +243,7 @@ class Perceptron:
 
 # FIN DE LA CLASSE PERCEPTRON
 
-def learningStage(Perceptron):
+def learningStage(p):
     # n = nombre d'images dans le tableau d'apprentissage
     n = numpy.shape(data[0][0])[0]
     # on créé un vecteur de (ITERATIONS,) valeurs entières prises aléatoirement entre 0 et n-1
@@ -242,7 +255,7 @@ def learningStage(Perceptron):
     print "FIN DE LA PHASE D'APPRENTISSAGE"
 # FIN FONCTION learningStage
 
-def testStage(Perceptron):
+def testStage(p):
     print "DEBUT DE LA PHASE DE TEST SUR", TEST_ITERATIONS, "ITERATIONS"
     # n = nombre d'images dans le tableau de test
     n = numpy.shape(data[1][0])[0]
@@ -251,6 +264,7 @@ def testStage(Perceptron):
     # i va valoir itérativement les valeurs dans indices / NB on aurait aussi pu écrire "for j in xrange(10): i = indices[j]"
     for i in indices:
         p.test(i)
+    print 
     print "LUS :", p.nb_read, "BONS :", p.nb_right, "FAUX :", p.nb_wrong
     e = (p.nb_wrong / p.nb_read) * 100
     print "POURCENTAGE D'ERREUR :", e
